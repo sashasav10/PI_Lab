8b1b2954f04d6fd820fd6dbd4f8bde09c8a1c3bd
Oleksandr Saveliev <sasha.sav.10@gmail.com>
2022-11-12 21:13:36 +0100

    added lab 2

---
+++ oblasti.php
@@ -0,0 +1,40 @@
+<?php
+$fp = file_get_contents("./oblinfo.txt");
+$lines = explode("\r\n", $fp);
+$count = 0;
+$total = 0;
+?>
+<table class="text-center m-auto" border="2">
+    <thead>
+        <tr>
+            <td>№</td>
+            <td>Область</td>
+            <td>Населення, тис</td>
+            <td>Число вузів</td>
+            <td>Число вузів на 100 тис населення</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        foreach ($lines as $key => $value) {
+            if ($key != 0) {
+                if ($key % 3 == 1) { $count++;
+        ?>
+            <tr>
+                <td>
+                    <?= $count; ?>
+                </td>
+                <?php } ?>
+                <td><?= $value; ?>
+                </td>
+                <?php
+                if ($key % 3 == 2) $total = $value;
+                if ($key % 3 == 0) {
+                ?>
+                <td>
+                    <?= round($value * 100 / $total, 2); ?>
+                </td>
+            </tr>
+        <?php } } } ?>
+    </tbody>
+</table>
