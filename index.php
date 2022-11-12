e31d0d992a254cc7c99d63048673004b1f894893
Oleksandr Saveliev <sasha.sav.10@gmail.com>
2022-11-13 00:55:35 +0100

    lab 2

--- index.php
+++ index.php
@@ -19,10 +19,6 @@
 	.title {
 		text-align: center;
 	}
-	.container-buttons {
-		 width:100%;
-}
-
 	.text {
 		margin-left: 10%;
 		margin-right: 10%;
@@ -70,14 +66,14 @@ Software engineering, Cherkasy  (Unfinished higher), from 2019 to 2023 (4 year
 					<li class="il">Android developemnet. Ekreative edu (2022)</li>
 					<li class="il">Kotlin developemnet. Triare education (2022)</li>
 				</ul>
-			</div class="container-buttons">
+			<p>
 				<form action="sendEmail.php">
 					<button class="btn" type="submit">send email </button>
 				</form>
 				<form action="oblasti.php">
 					<button class="btn" type="submit">show info by region </button>
 				</form>
-		</div>
+	</p>
 	</section>
 </body>
 </html>
