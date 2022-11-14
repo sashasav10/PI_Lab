badaf810f3aa5a2b666c31ef9e93447a09d2a3ef
Oleksandr Saveliev <sasha.sav.10@gmail.com>
2022-11-14 23:56:09 +0100

    lab 5

--- sinoptik.php
+++ sinoptik.php
@@ -23,17 +23,17 @@
       $content = file_get_html('https://www.gismeteo.ua/ua/weather-kharkiv-5053');
       $city=$content ->find('div[class=page-title]')[0]->plaintext;
       $date=$content ->find('div[class=localedate]')[0]->innertext;
-      $sunrise=$content ->find('div[class=astro-times]')[1]->innertext;
-      $sunset=$content ->find('div[class=astro-times]')[2]->innertext;
+      $sunrise=$content ->find('div[class=astro-sun-top]')[1]->innertext;
+      $sunset=$content ->find('div[class=astro-sun-top]')[2]->innertext;
       $daytime=$content ->find('div[class=astro-progress]')[0]->innertext;
-      $weathertemp=$content ->find('//div[class=widget-row-chart widget-row-chart-temperature]//div[class=chart]//div[class=values]')[0]->outertext;
+      //$weathertemp=$content ->find('//div[class=widget-row-chart widget-row-chart-temperature]//div[class=chart]//div[class=values]')[0]->outertext;
 
       //echo "$city";
       echo "$date";
       echo "$sunrise";
       echo "$sunset";
       echo "$daytime";
-      echo "$weathertemp";
+    //  echo "$weathertemp";
       // $date;
       //preg_match('date date-1(.*?)', $content, $match);
       //$in_stock = $match[1];
