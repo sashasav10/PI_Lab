43efae922be4bcb6e3849f0a7c49801359dc6079
Oleksandr Saveliev <sasha.sav.10@gmail.com>
2022-11-14 22:48:58 +0100

    lab 5

--- sinoptik.php
+++ sinoptik.php
@@ -1,8 +1,15 @@
 <?php
 include("simple_html_dom.php");
 $content = file_get_html('https://www.gismeteo.ua/ua/weather-kharkiv-5053');
+$city=$content ->find('div[class=page-title]')[0]->innertext;
 $date=$content ->find('div[class=date date-1]')[0]->innertext;
+$sunrise=$content ->find('div[class=astro-progress]')[0]->innertext;
+//$sunset=$content ->find('div[class=date date-1]')[0]->innertext;
+//$daytime=$content ->find('div[class=date date-1]')[0]->innertext;
+
+echo $city;
 echo $date;
+echo $sunrise;
 // $date;
 //preg_match('date date-1(.*?)', $content, $match);
 //$in_stock = $match[1];
