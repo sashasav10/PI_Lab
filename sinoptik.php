ce8c6f459a3d4f839713503e5c5deab24758610e
Oleksandr Saveliev <sasha.sav.10@gmail.com>
2022-11-14 21:24:22 +0100

    lab 5

--- sinoptik.php
+++ sinoptik.php
@@ -1,10 +1,11 @@
 <?php
-$content = file_get_contents('http://www.sparkfun.com/commerce/product_info.php?products_id=9279');
+$content = file_get_contents('https://www.gismeteo.ua/ua/weather-kharkiv-5053/');
 
-preg_match('#<tr><th>(.*)</th> <td><b>price</b></td></tr>#', $content, $match);
+preg_match('date date-1', $content, $match);
 $price = $match[1];
 
-preg_match('#<input type="hidden" name="quantity_on_hand" value="(.*?)">#', $content, $match);
-$in_stock = $match[1];
+//preg_match('#<input type="hidden" name="quantity_on_hand" value="(.*?)">#', $content, $match);
+//$in_stock = $match[1];
 
-echo "Price: $price - Availability: $in_stock\n";
+echo "Price: $price - ";
+//Availability: $in_stock\n
