572b03b9d66e18c2d9f109c24106dfc7c8b74d48
Oleksandr Saveliev <sasha.sav.10@gmail.com>
2022-11-14 23:46:07 +0100

    lab 5

--- sinoptik.php
+++ sinoptik.php
@@ -6,17 +6,16 @@
   $sunrise=$content ->find('div[class=astro-times]')[1]->innertext;
   $sunset=$content ->find('div[class=astro-times]')[2]->innertext;
   $daytime=$content ->find('div[class=astro-progress]')[0]->innertext;
-  $weathertime=$content ->find('div[class=widget-row widget-row-time]')[0]->innertext;
-  $weathericon=$content ->find('div[class=widget-row widget-row-icon]')[0]->innertext;
-  $weathertemp=$content ->find('div[class=widget-row-chart widget-row-chart-temperature]')[0]->innertext;
+  $weathertime=$content ->find('div[class=widget-row widget-row-time]')[0]->outertext;
+  $weathertemp=$content ->find('div[class=widget-row-chart widget-row-chart-temperature]')[0]->outertext;
 
-  echo "$city";
+  //echo "$city";
+  <p> Kharkiv</p>
   echo "$date";
   echo "$sunrise";
   echo "$sunset";
   echo "$daytime";
   echo "$weathertime";
-  echo "$weathericon";
   echo "$weathertemp";
   // $date;
   //preg_match('date date-1(.*?)', $content, $match);
