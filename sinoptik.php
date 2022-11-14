a53121cdd513f990550fa523f4caea4d9e5a0af7
Oleksandr Saveliev <sasha.sav.10@gmail.com>
2022-11-14 21:54:17 +0100

    lab 5

--- sinoptik.php
+++ sinoptik.php
@@ -3,7 +3,7 @@ include(./additional/simplehtmldom_1_9_1/simple_html_dom.php);
 $content = file_get_html('https://www.gismeteo.ua/ua/weather-kharkiv-5053');
 $date=$content->find("div#date date-1",0)->innertext;
 
-echo "0 $date";
+echo $date;
 
 //preg_match('#<input type="hidden" name="quantity_on_hand" value="(.*?)">#', $content, $match);
 //$in_stock = $match[1];
