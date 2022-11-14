dc1d15c8d06eb64edfbb7ae3b970cc15f40fdf9a
Oleksandr Saveliev <sasha.sav.10@gmail.com>
2022-11-14 21:49:16 +0100

    lab 5

--- sinoptik.php
+++ sinoptik.php
@@ -1,17 +1,9 @@
 <?php
-//Users/oleksandrsaveliev/Documents/4 курс/Програмування інтернет/Lab PI 2022/additional/simplehtmldom_1_9_1
-$content = file_get_contents('https://www.gismeteo.ua/ua/weather-kharkiv-5053/');
+include(./additional/simplehtmldom_1_9_1/simple_html_dom.php)
+$content = file_get_html('https://www.gismeteo.ua/ua/weather-kharkiv-5053');
+$date=$content->find("div#date date-1",0)->innertext
 
-preg_match('date date-1', $content, $match);
-$price = $match[1];
-echo "0 $match[0]";
-echo "1 $match[1]";
-echo "2 $match[2]";
-echo "3 $match[3]";
-echo "4 $match[4]";
-echo "5 $match[5]";
-echo "6 $match[6]";
-echo "7 $match[7]";
+echo "0 $date";
 
 //preg_match('#<input type="hidden" name="quantity_on_hand" value="(.*?)">#', $content, $match);
 //$in_stock = $match[1];
