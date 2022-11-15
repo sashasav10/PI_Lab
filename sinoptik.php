70a7f9011fb94e5012e8967d29686afe1aa2177e
Oleksandr Saveliev <sasha.sav.10@gmail.com>
2022-11-15 20:51:16 +0100

    lab 5

--- sinoptik.php
+++ sinoptik.php
@@ -23,17 +23,13 @@
       $content = file_get_html('https://www.gismeteo.ua/ua/weather-kharkiv-5053');
       $city=$content ->find('div[class=page-title]')[0]->plaintext;
       $date=$content ->find('div[class=localedate]')[0]->innertext;
-      $sunrise=$content ->find('div[class=astro-sun-top]')[0]->innertext;
-    //  $sunset=$content ->find('div[class=astro-sun-top]')[2]->innertext;
-      $daytime=$content ->find('div[class=astro-progress]')[0]->innertext;
-      //$weathertemp=$content ->find('//div[class=widget-row-chart widget-row-chart-temperature]//div[class=chart]//div[class=values]')[0]->outertext;
+      $daytime=$content ->find('div[class=astro-sun-top]')[0]->innertext;
+      $temperature=$content ->find('div[class=widget-row-chart widget-row-chart-temperature]')[0]->innertext;
 
       //echo "$city";
       echo "$date";
-      echo "$sunrise";
-      echo "$sunset";
       echo "$daytime";
-    //  echo "$weathertemp";
+  echo "$temperature";
       // $date;
       //preg_match('date date-1(.*?)', $content, $match);
       //$in_stock = $match[1];
@@ -42,7 +38,6 @@
       //Availability: $in_stock\n
       ?>
   	<section class="text-section">
-  			]
   	</section>
   </body>
   </html>
