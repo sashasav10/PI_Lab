378c70d3563b2affa52ffd99f8ec16c1e561ff58
Oleksandr Saveliev <sasha.sav.10@gmail.com>
2022-11-21 19:50:27 +0100

    fixed bugs lab 7

--- handle_search.php
+++ handle_search.php
@@ -19,14 +19,13 @@ curl_close($ch);
 
 // parse the html using regex
 preg_match_all(
-    "/<div class=\"listing__body-wrap image-switch\"(.*)<\/div>
-    <nav class=\"listing__pagination\" /s",
+    "/<div class=\"listing__body-wrap image-switch\"(.*)<nav class=\"listing__pagination\" /s",
     $rozetka_html,
     $matches
 );
 
 if (!empty($matches[0][0])) {
-    echo $matches[0][0];
+    echo $matches;
 } else {
     echo "$search_term not found";
 }
