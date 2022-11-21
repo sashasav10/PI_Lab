e019b9a09ebea0906783e66a18d782195ca6761a
Oleksandr Saveliev <sasha.sav.10@gmail.com>
2022-11-21 21:48:18 +0100

    fixed bugs lab 7

--- handle_search.php
+++ handle_search.php
@@ -18,15 +18,14 @@ $rozetka_html = curl_exec($ch);
 curl_close($ch);
 
 // parse the html using regex
-preg_match_all('/<div[^>]+class=\"listing__body-wrap image-switch\"[^>]*>(.*)<\/div>/',
+preg_match_all(
+    "/<div class=\"listing__body-wrap image-switch\"(.*?)<\/div>/s",
     $rozetka_html,
     $matches
 );
-echo "<pre>";
-print($matches[1]);
-echo "</pre>";
-//if (!empty($matches[0][0])) {
-  //  echo $matches[0][0];
-//} else {
-  //  echo "$search_term not found";
-//}
+
+if (!empty($matches[0][0])) {
+    echo $matches[0][0];
+} else {
+    echo "$search_term not found";
+}
