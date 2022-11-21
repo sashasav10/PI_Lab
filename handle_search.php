1153cacf9f336e6c3466b8efd12693853aab2c65
Oleksandr Saveliev <sasha.sav.10@gmail.com>
2022-11-21 22:57:55 +0100

    fixed bugs lab 7

--- handle_search.php
+++ handle_search.php
@@ -23,7 +23,7 @@ preg_match_all(
     $rozetka_html,
     $matches
 );
-$matches[0][0] = substr($matches[0][0], 0, 119186);
+$matches[0][0] = substr($matches[0][0], 0, 119170);
 if (!empty($matches[0][0])) {
     echo $matches[0][0];
 } else {
